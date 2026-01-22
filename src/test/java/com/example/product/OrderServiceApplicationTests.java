b7449470505e5537e891262a24408cbf5dd97955
Saurabh Kumar <saurkumar@estuate.com>
2026-01-22 11:42:11 +0530

    update changes

--- src/test/java/com/example/product/OrderServiceApplicationTests.java
+++ src/test/java/com/example/product/OrderServiceApplicationTests.java
@@ -4,7 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+		  properties = {
+		    "spring.cloud.config.enabled=false",
+		    "spring.autoconfigure.exclude=" +
+		      "org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration," +
+		      "org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration"
+		  }
+		)
 @ActiveProfiles("test")
 class OrderServiceApplicationTests {
 
