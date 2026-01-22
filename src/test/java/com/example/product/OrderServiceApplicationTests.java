c328400bb8cc11b67270b8786c69f62071d2ca1f
Saurabh Kumar <saurkumar@estuate.com>
2026-01-22 11:57:57 +0530

    update second time changes

--- src/test/java/com/example/product/OrderServiceApplicationTests.java
+++ src/test/java/com/example/product/OrderServiceApplicationTests.java
@@ -2,21 +2,27 @@ package com.example.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-		  properties = {
-		    "spring.cloud.config.enabled=false",
-		    "spring.autoconfigure.exclude=" +
-		      "org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration," +
-		      "org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration"
-		  }
-		)
-@ActiveProfiles("test")
-class OrderServiceApplicationTests {
+import com.example.product.repository.OrderRepository;
 
-	@Test
-	void contextLoads() {
+@SpringBootTest(
+	    properties = {
+	        "spring.cloud.config.enabled=false",
+	        "spring.autoconfigure.exclude=" +
+	          "org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration," +
+	          "org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration"
+	    }
+	)
+	@ActiveProfiles("test")
+	class OrderServiceApplicationTests {
+
+	    @MockBean
+	    private OrderRepository orderRepository;
+
+	    @Test
+	    void contextLoads() {
+	    }
 	}
 
-}
